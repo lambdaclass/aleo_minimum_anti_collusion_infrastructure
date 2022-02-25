fc4313645f04717dff96a162a8fb58f48a30169a
Mati Onorato <monorato@forte.io>
2022-02-25 12:57:11 -0300

    add warp test endpoint for election creation

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+mod models;
+
+use self::models::Election;
+
+#[tokio::main] // 2.
+async fn main() {
+    let hello = warp::path!("election" / i64 / i64) // 3.
+        .map(|sign_up_duration, voting_duration| {
+            warp::reply::json(&Election::new(sign_up_duration, voting_duration))
+        }); // 4.
+
+    warp::serve(hello) // 5.
+        .run(([127, 0, 0, 1], 3000)) // 6.
+        .await;
 }
