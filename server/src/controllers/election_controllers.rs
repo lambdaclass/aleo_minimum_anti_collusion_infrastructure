edb055ec71f92931209ad7c2280dcf6a665a2bce
Mati Onorato <monorato@forte.io>
2022-03-07 14:42:27 -0300

    add endpoints

--- server/src/controllers/election_controllers.rs
+++ server/src/controllers/election_controllers.rs
@@ -1,5 +1,6 @@
 use crate::models::Election;
 use serde::Deserialize;
+use serde_json::json;
 use warp::reply::Json;
 
 #[derive(Debug, Deserialize)]
@@ -8,8 +9,28 @@ pub struct ElectionCreate {
     voting_duration: i64,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct ElectionSignUp {
+    public_key: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ElectionMsg {
+    public_key_old: String,
+    public_key_new: String,
+    vote_option: String,
+}
+
 pub async fn create(data: ElectionCreate) -> Result<Json, warp::Rejection> {
     let election = Election::new(data.sign_up_duration, data.voting_duration);
 
     Ok(warp::reply::json(&election))
+}
+
+pub async fn sign_up(data: ElectionSignUp) -> Result<Json, warp::Rejection> {
+    Ok(warp::reply::json(&json!({"msg":"not implemented"})))
+}
+
+pub async fn receive_msg(data: ElectionMsg) -> Result<Json, warp::Rejection> {
+    Ok(warp::reply::json(&json!({"msg":"not implemented"})))
 }
--- server/src/filters/election_filters.rs
+++ server/src/filters/election_filters.rs
@@ -6,9 +6,23 @@ pub fn get_filters() -> impl Filter<Extract = impl warp::Reply, Error = warp::Re
 
     let create = election_base
         .and(warp::post())
-        .and(warp::body::json())
         .and(warp::path::end())
+        .and(warp::body::json())
         .and_then(election_controllers::create);
 
-    create
+    let sign_up = election_base
+        .and(warp::post())
+        .and(warp::path("sign_up"))
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .and_then(election_controllers::sign_up);
+
+    let msg = election_base
+        .and(warp::post())
+        .and(warp::path("msg"))
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .and_then(election_controllers::receive_msg);
+
+    create.or(sign_up).or(msg)
 }
