0d2701508e02317062a918a71fd1bc789402a4f1
MauroFab <maurotoscano2@gmail.com>
2022-03-17 14:39:29 -0300

    Clean code

--- client/src/transactions/transactions.rs
+++ client/src/transactions/transactions.rs
@@ -6,8 +6,6 @@ mod tests {
         dpc::{testnet2::Testnet2, Ledger, LedgerProof, Record},
         prelude::{Address, Function, LedgerTree, Payload, PrivateKey, Transaction, ViewKey},
         traits::LedgerTreeScheme,
-        traits::Network,
-        utilities,
         utilities::ToBytes,
     };
 
@@ -57,8 +55,6 @@ mod tests {
 
     #[test]
 
-    //Note this version tried a NOOP + Dummy, and inner circuit failed
-    //Dummies can't have a non zero payload
     fn local_transaction_with_data_test() {
         //This test lasts 25 min approx
         println!("Creating ledger ...");
@@ -105,41 +101,24 @@ mod tests {
     ) -> dpc::Request<Testnet2> {
         let mut rng = StdRng::from_entropy();
 
-        // Sample a burner noop private key.
-        // We should  change it for the app address later
         let noop_address = Address::from_private_key(&private_key);
 
         //Remember 2: Payload size in testnet2
         //const RECORD_PAYLOAD_SIZE_IN_BYTES: usize = 128;
-
         let byte_data: Vec<u8> = vec![42; 128];
 
-        let payload_data1 = Payload::<Testnet2>::from(&byte_data);
+        //Since we will use an insert function later
+        //We will only use one payload data,
+        //The other is a noop
+        let payload_data = Payload::<Testnet2>::from(&byte_data);
 
-        let payload_data2 = Payload::<Testnet2>::from(&byte_data);
-
-        let record_with_data1 = Record::new_input(
-            noop_address,
-            0,
-            payload_data1,
-            *<Testnet2>::noop_program_id(),
-            utilities::UniformRand::rand(&mut rng),
-            utilities::UniformRand::rand(&mut rng),
-        )
-        .unwrap();
-
-        let noop_record2 = Record::new_noop_input(noop_address, &mut rng).unwrap();
+        let noop_record = Record::new_noop_input(noop_address, &mut rng).unwrap();
 
         //Remember: Testnet2 always have 2 input registers
         //And two outputs!
         let mut records = Vec::with_capacity(2);
 
-        records.push(noop_record2);
-
-        //let mut records_test = Vec::with_capacity(2);
-
-        //records_test.push(record_with_data1);
-        //records_test.push(noop_record2);
+        records.push(noop_record);
 
         let ledger_proof1 = LedgerProof::<Testnet2>::default();
         let ledger_proof2 = LedgerProof::<Testnet2>::default();
@@ -150,7 +129,7 @@ mod tests {
             &noop_address,
             &noop_address,
             amount,
-            payload_data2,
+            payload_data,
         );
 
         let operation = dpc::Operation::<Testnet2>::Evaluate(
@@ -173,6 +152,7 @@ mod tests {
         return request;
     }
 
+    //WIP
     fn rcp_transaction_test() {
         //This test lasts 25 min approx
 
