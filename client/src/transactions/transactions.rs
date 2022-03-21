9b72e9c227becc2600645abc24999da59f6dddef
MauroFab <maurotoscano2@gmail.com>
2022-03-21 19:47:02 -0300

    Add store data variants with events

--- client/src/transactions/transactions.rs
+++ client/src/transactions/transactions.rs
@@ -10,6 +10,7 @@ use snarkvm::{
 pub fn create_store_data_request(
     private_key: PrivateKey<Testnet2>,
     data: Vec<u8>,
+    is_public: bool,
 ) -> dpc::Request<Testnet2> {
     let mut rng = StdRng::from_entropy();
 
@@ -49,7 +50,7 @@ pub fn create_store_data_request(
         ledger_proofs,
         operation,
         dpc::AleoAmount::ZERO,
-        false,
+        is_public,
         &mut rng,
     )
     .unwrap();
@@ -63,14 +64,13 @@ mod tests {
     use rand::{rngs::StdRng, SeedableRng};
     use snarkvm::{
         dpc,
-        dpc::{testnet2::Testnet2, Ledger, LedgerProof},
-        prelude::{Address, PrivateKey, Transaction, ViewKey},
+        dpc::{testnet2::Testnet2, Ledger, LedgerProof, VirtualMachine},
+        prelude::{Address, Event, PrivateKey, Transaction, ViewKey},
     };
 
     use std::sync::atomic::AtomicBool;
 
     #[test]
-
     ///Creates a transaction and submits it to a local ledger
     /// This test lasts about 25 min
     fn local_transaction_test() {
@@ -119,7 +119,7 @@ mod tests {
         //This request is in fact, a transition
         let mut rng = StdRng::from_entropy();
         let new_private_key = PrivateKey::new(&mut rng);
-        let request = create_store_data_request(new_private_key.clone(), vec![42; 128]);
+        let request = create_store_data_request(new_private_key.clone(), vec![42; 128], false);
         println!("Done");
         let mut rand2 = StdRng::from_entropy();
         println!("Creating transaction ...");
@@ -145,5 +145,40 @@ mod tests {
         let decrypted_records =
             chain_transaction.to_decrypted_records(&ViewKey::from_private_key(&new_private_key));
         println!("Decrypted record 0: {}", decrypted_records[0]);
+    }
+
+    #[test]
+    fn local_transaction_with_public_data_test() {
+        //This test lasts 25 min approx
+        println!("Creating ledger ...");
+        let mut ledger = Ledger::<Testnet2>::new().unwrap();
+        println!("Done");
+        println!("Creating request ...");
+        //This request is in fact, a transition
+        let mut rng = StdRng::from_entropy();
+        let new_private_key = PrivateKey::new(&mut rng);
+        let request = create_store_data_request(new_private_key.clone(), vec![42; 128], true);
+        println!("Done");
+        let mut rand2 = StdRng::from_entropy();
+        println!("Creating transaction ...");
+        let transaction =
+            Transaction::new(ledger.to_ledger_tree().clone(), &request, &mut rand2).unwrap();
+        println!("Done");
+        println!("Adding unconfirmed transaction ...");
+        ledger.add_unconfirmed_transaction(&transaction).unwrap();
+        println!("Done");
+        let mut rand64 = StdRng::from_entropy();
+        let new_account: Address<Testnet2> =
+            Address::from_private_key(&PrivateKey::<Testnet2>::new(&mut rand64));
+        let mut rand3 = StdRng::from_entropy();
+        let y = ledger.latest_block().unwrap();
+        println!("Latest block before transaction: {}", y.to_string());
+        ledger
+            .mine_next_block(new_account, &AtomicBool::new(false), &mut rand3)
+            .unwrap();
+        let x = ledger.latest_block().unwrap();
+        let chain_transaction = x.transactions().last().unwrap();
+        println!("Block: {}", x.to_string());
+        println!("Transaction {}", chain_transaction.to_string());
     }
 }
