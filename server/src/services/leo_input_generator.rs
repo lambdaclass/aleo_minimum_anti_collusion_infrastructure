bba67fb3374a0bd72e5096f4cba47b725b0966d4
MauroFab <maurotoscano2@gmail.com>
2022-04-10 00:42:53 -0300

    Add input generator

--- server/src/services/leo_input_generator.rs
+++ server/src/services/leo_input_generator.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::str;
-const MAX_VOTES:usize = 32;
+const MAX_VOTES: usize = 32;
 const TALLY_IN_FILE_PATH: &str = "./../circuits/tally/inputs/tally.in";
 
-pub fn generate_input_file(votes: [u32; MAX_VOTES],vote_merkle_root: &str){
+pub fn generate_input_file(votes: [u32; MAX_VOTES], vote_merkle_root: &str) {
     /*
 
     This a template of the current input for a Leo program
@@ -13,17 +13,17 @@ pub fn generate_input_file(votes: [u32; MAX_VOTES],vote_merkle_root: &str){
 
     [main]
     votes_as_u32: [u32; 32] = [
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2 
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2
         ];
 
     votes_as_field: [field; 32] = [
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2 
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2,
+            1, 2, 3, 2, 2, 3, 1, 2
         ];
 
     votes_merkle_root: field = 6081127065217055003429398673533374549058098389318475736416753929574343365699;
@@ -33,8 +33,7 @@ pub fn generate_input_file(votes: [u32; MAX_VOTES],vote_merkle_root: &str){
     */
 
     let votes_strs = format!("{:?}", votes);
-    let mut input_as_string: String =
-        String::from("[main]\nvotes_as_u32: [u32; 32] = ");
+    let mut input_as_string: String = String::from("[main]\nvotes_as_u32: [u32; 32] = ");
     input_as_string.push_str(votes_strs.as_str());
     input_as_string.push_str(";\n");
     input_as_string.push_str("votes_as_field: [field; 32] = ");
@@ -55,14 +54,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_generate_input(){
+    fn test_generate_input() {
         let votes: [u32; 32] = [
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2, 
-            1, 2, 3, 2, 2, 3, 1, 2 
+            1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2,
+            3, 1, 2,
         ];
-        generate_input_file(votes, "6081127065217055003429398673533374549058098389318475736416753929574343365699");
+        generate_input_file(
+            votes,
+            "6081127065217055003429398673533374549058098389318475736416753929574343365699",
+        );
         assert!(true);
     }
 }
