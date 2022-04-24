c42f2fa8e04a6490642352a58de2b97bd132b9ce
Mati Onorato <monorato@forte.io>
2022-04-24 16:52:20 +0200

    - get all the votes from the ledger - generates the fixed array input for the tally circuit - invalid votes wwould be count as the 0 option

--- libs/src/rcp/mod.rs
+++ libs/src/rcp/mod.rs
@@ -1,3 +1,3 @@
 #![allow(clippy::module_inception)]
 pub mod rcp;
-pub use rcp::sync_spray_transaction;
+pub use rcp::*;
--- server/src/controllers/election_controllers.rs
+++ server/src/controllers/election_controllers.rs
@@ -2,7 +2,7 @@ use crate::r2d2::Pool;
 use crate::services::leo_io::generate_input_file;
 use crate::RedisConnectionManager;
 use crate::{models::Election, services::leo_io};
-use aleo_maci_libs::merkle_tree::generate_merkle_root;
+use aleo_maci_libs::{merkle_tree::generate_merkle_root, rcp::get_transaction_public_data};
 use ff::PrimeField;
 use poseidon_rs::Fr;
 
@@ -12,6 +12,7 @@ use serde_json::json;
 use std::process::Command;
 use warp::reject::Reject;
 use warp::reply::Json;
+
 #[derive(Debug, Deserialize)]
 pub struct ElectionCreate {
     sign_up_duration: i64,
@@ -70,38 +71,62 @@ struct DBError;
 impl warp::reject::Reject for DBError {}
 
 pub async fn start_tally(pool: Pool<RedisConnectionManager>) -> Result<Json, warp::Rejection> {
-    //TO DO: Get the votes from the blockchain
-    let votes = [
-        1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2, 3, 1, 2, 1, 2, 3, 2, 2, 3,
-        1, 2,
-    ];
-
     const MAX_AMOUNT_OF_VOTES: isize = 32;
 
+    // get redis pool connection
     let mut con = match pool.get() {
         Ok(v) => v,
         Err(_) => return Err(warp::reject::custom(DBError)),
     };
 
-    let votes_from_pool: Vec<String> = match con.lrange("votes", 0, MAX_AMOUNT_OF_VOTES - 1) {
+    // get transaction id from redis db
+    let votes_ids_from_pool: Vec<String> = match con.lrange("votes", 0, MAX_AMOUNT_OF_VOTES - 1) {
         Ok(v) => v,
         Err(_) => return Err(warp::reject::custom(DBError)),
     };
 
-    println!("Votes from pool : {:?}", votes_from_pool);
+    println!(
+        "Aleo Transactions to be computed: {:?}",
+        votes_ids_from_pool
+    );
 
-    //TO DO: Calculate the merkle root with the votes
+    // Get votes from the Aleo Ledger
+    let votes: Vec<String> = votes_ids_from_pool
+        .iter()
+        .map(|x| {
+            get_transaction_public_data(x.to_string()).expect("A vote was not found in the ledger")
+        })
+        .collect();
+
+    // Generate markle root of the votes for the circuit input
     let votes_merkle_root_fr_str = generate_merkle_root(
         votes
+            .iter()
             .map(|v| Fr::from_str(&v.to_string()).unwrap())
-            .to_vec(),
+            .collect(),
     )
     .to_string();
 
     let votes_merkle_root_leo_str = leo_io::fr_string_to_leo_str(votes_merkle_root_fr_str);
 
-    leo_io::generate_input_file(votes, votes_merkle_root_leo_str.as_str());
+    // Transform votes to a fixed array of MAX_AMOUNT_OF_VOTES elements
+    // if there is and invalid vote option, it will be count as 0
+    // if there is less than MAX_AMOUNT_OF_VOTES, the reamaning votes will be count as 0
+    let mut votes_fixed: [u32; MAX_AMOUNT_OF_VOTES as usize] = [0; MAX_AMOUNT_OF_VOTES as usize];
+    for i in 0..MAX_AMOUNT_OF_VOTES {
+        votes_fixed[i as usize] = match votes.get(i as usize) {
+            Some(v) => match v.parse::<u32>() {
+                Ok(v) => v,
+                Err(_) => 0,
+            },
+            None => 0,
+        };
+    }
 
+    // Generate circuit input file
+    generate_input_file(votes_fixed, votes_merkle_root_leo_str.as_str());
+
+    // Run circuit
     //TO DO: Make async, don't run if it's already running or has run before
     Command::new("sh")
         .arg("-c")
