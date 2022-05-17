996e9e7e0114d41f1ab805fc4e9a885c1eac9cf9
Mati Onorato <monorato@forte.io>
2022-05-17 15:47:57 -0300

    remove unused assets

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {root as App} from './App.bs';
 import reportWebVitals from './reportWebVitals';
 
