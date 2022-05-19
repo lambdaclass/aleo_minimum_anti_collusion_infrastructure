2a40cfed1c42e61a204da3c6c85b236fe28f8089
MauroFab <maurotoscano2@gmail.com>
2022-05-19 19:46:48 -0300

    Generate full merkle tree

--- libs/src/merkle_tree/merkle_tree.rs
+++ libs/src/merkle_tree/merkle_tree.rs
@@ -1,5 +1,6 @@
 use poseidon_rs::Fr;
 use poseidon_rs::Poseidon;
+use std::fmt;
 
 pub fn hash(v1: Fr, v2: Fr) -> Fr {
     let mut big_arr: Vec<Fr> = Vec::new();
@@ -9,6 +10,8 @@ pub fn hash(v1: Fr, v2: Fr) -> Fr {
     poseidon.hash(big_arr).unwrap()
 }
 
+/// Generates the merkle root of the givven leaves using Poseidon algorithm
+/// Compatible with Leo MACI circuits
 pub fn generate_merkle_root(leaves: Vec<Fr>) -> Fr {
     let mut leaves = leaves.clone();
 
@@ -26,6 +29,48 @@ pub fn generate_merkle_root(leaves: Vec<Fr>) -> Fr {
     }
     leaves[0]
 }
+
+fn is_power_of_two(x: usize) -> bool {
+    return (x & (x - 1)) == 0;
+}
+
+#[derive(Debug, Clone)]
+struct SizeNotPowerOfTwoError;
+
+impl fmt::Display for SizeNotPowerOfTwoError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "MerkleTree only supports power of two leaves")
+    }
+}
+
+struct MerkleTree {
+    pub nodes: Vec<Fr>,
+}
+
+impl MerkleTree {
+    pub fn new(leaves: Vec<Fr>) -> Result<Self, SizeNotPowerOfTwoError> {
+        if !is_power_of_two(leaves.len()) {
+            return Err(SizeNotPowerOfTwoError);
+        }
+        let mut nodes = leaves;
+        let amount_of_leaves = nodes.len();
+        let tree_height = (nodes.len() as f32).log2().round() as usize;
+        let mut offset = 0;
+        //For each level
+        for level in 1..tree_height + 1 {
+            if (level != 1) {
+                offset = offset + (amount_of_leaves / (2usize.pow(level as u32 - 2)));
+            }
+            //For each node in the level
+            for j in 0..(amount_of_leaves / (2usize.pow(level as u32))) {
+                let new_node = hash(nodes[j * 2 + offset], nodes[j * 2 + 1 + offset]);
+                nodes.push(new_node);
+            }
+        }
+        Ok(Self { nodes })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use ff::PrimeField;
@@ -52,6 +97,25 @@ mod tests {
 
         assert_eq!(
             generate_merkle_root(votes.to_vec()).to_string(),
+            "Fr(0x0d71cbc322578e133085b861a656d34b3abc2cc65ac11d24618aa53d49e5d443)"
+        );
+    }
+
+    #[test]
+    fn merkle_tree_root_test() {
+        let one: Fr = Fr::from_str("1").unwrap();
+        let two: Fr = Fr::from_str("2").unwrap();
+        let three: Fr = Fr::from_str("3").unwrap();
+
+        let votes: [Fr; 32] = [
+            one, two, three, two, two, three, one, two, one, two, three, two, two, three, one, two,
+            one, two, three, two, two, three, one, two, one, two, three, two, two, three, one, two,
+        ];
+
+        let tree = MerkleTree::new(votes.to_vec()).unwrap();
+        let root = tree.nodes.last().unwrap();
+        assert_eq!(
+            root.to_string(),
             "Fr(0x0d71cbc322578e133085b861a656d34b3abc2cc65ac11d24618aa53d49e5d443)"
         );
     }
