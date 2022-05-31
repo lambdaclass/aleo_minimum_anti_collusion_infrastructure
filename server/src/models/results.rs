af683b83c6edf1efd76170f6395d11c52f35d2f2
MauroFab <maurotoscano2@gmail.com>
2022-05-31 15:31:22 -0300

    Remove unused code

--- server/src/models/results.rs
+++ server/src/models/results.rs
@@ -9,13 +9,4 @@ impl Results {
     pub fn new(results: Vec<u32>) -> Self {
         Self { results }
     }
-
-    pub fn new_from_string(results: Vec<String>) -> Self {
-        Self {
-            results: results
-                .into_iter()
-                .map(|r| r.parse::<u32>().unwrap())
-                .collect(),
-        }
-    }
 }
